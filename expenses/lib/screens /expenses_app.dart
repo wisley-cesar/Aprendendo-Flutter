59dab49442a33eaa8d317d77d6ff446069634a41
Wisley César <wisleycesar32@gmail.com>
2024-06-12 14:20:08 -0300

    Tela

---
+++ expenses/lib/screens /expenses_app.dart
@@ -0,0 +1,41 @@
+import 'package:expenses/screens%20/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+final ThemeData tema = ThemeData();
+
+class ExpensesAPP extends StatelessWidget {
+  const ExpensesAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              button: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
