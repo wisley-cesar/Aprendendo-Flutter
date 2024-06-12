bbc9e9723978196dd590ad8b7b5e40433c37f358
Wisley César <wisleycesar32@gmail.com>
2024-06-12 14:10:43 -0300

    Componentes Adaptativos

---
+++ expenses/lib/components/adaptative/adaptative_button.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdapativeButton extends StatelessWidget {
+  final String label;
+  final Function() onPressed;
+  final Color color;
+  final double size;
+  const AdapativeButton({
+    super.key,
+    required this.size,
+    required this.color,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: onPressed,
+            color: color,
+            child: Text(label),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+          )
+        : ElevatedButton(
+            onPressed: onPressed,
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(color),
+            ),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: size,
+              ),
+            ),
+          );
+  }
+}
---
+++ expenses/lib/components/adaptative/adaptative_date_piker.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AdaptativeDatePiker extends StatelessWidget {
+  final DateTime selectedDate;
+  final Function(DateTime) onDateChanged;
+  AdaptativeDatePiker(
+      {required this.selectedDate, required this.onDateChanged});
+
+  _showDatePicker(BuildContext context) {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2023),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      onDateChanged(pickedDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? Container(
+            height: 180,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: DateTime.now(),
+              minimumDate: DateTime(2019),
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: onDateChanged,
+            ),
+          )
+        : Container(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(selectedDate == null
+                    ? 'Nenhuma data selecionada!'
+                    : 'Data Selecionda: ${DateFormat('dd/MM/y').format(selectedDate)}'),
+                ElevatedButton(
+                  onPressed: () => _showDatePicker(context),
+                  child: const Expanded(
+                    child: Text(
+                      'Selecionar Data',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+  }
+}
